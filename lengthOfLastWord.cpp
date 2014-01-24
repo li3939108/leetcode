928b9ddc93417c6f90d5910774f3366e69778867
unknown <cnl2382@L219C01.cs.tamu.edu>
2014-01-24 13:13:45 -0600

    tab

--- lengthOfLastWord.cpp
+++ lengthOfLastWord.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int lengthOfLastWord(const char *s) {
-  int p = 0, i = 0, length = 0 ;
-  while(s[i] != 0){
-    if(s[i] == ' '){
-      length = i - p == 0 ? length : i - p ;
-      p = i + 1;
-    }
-    i++;
-  }
-  return i - p == 0 ? length : i - p;
+	int p = 0, i = 0, length = 0 ;
+	while(s[i] != 0){
+	if(s[i] == ' '){
+		length = i - p == 0 ? length : i - p ;
+		p = i + 1;
+	}
+	i++;
+	}
+	return i - p == 0 ? length : i - p;
 }
