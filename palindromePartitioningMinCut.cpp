a6f10a4413b624b2d03633c8bc22d4af4b637906
Fanchy <li3939108@gmail.com>
2014-02-06 01:14:33 -0600

    unresolved palindrompatitioningMinCut

--- palindromePartitioningMinCut.cpp
+++ palindromePartitioningMinCut.cpp
@@ -8,9 +8,42 @@ Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 
 */
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
 
 using namespace std;
+bool palindrome(string s ) {
+	int len = s.length();
+	if(len == 0 || len == 1 ){
+		return true;
+	}else if(s[0] == s[len - 1]){
+		return  palindrome(s.substr(1, len - 2));
+	}else{
+		return false;
+	}
+}
+int  minCut(string s) {
+	int len = s.length() ;
+	if(len == 0|| palindrome(s)){
+		return 0 ;
+	}else {
+		int dp[len + 1] ;
+		int i, j;
+		dp[len] = -1 ;
+		for(i = len - 1; i >= 0; i--){
+			dp[i] = len - i - 1;
+			for(j = i; j <= len - 1; j++){
+				if(palindrome(s.substr(i, j - i + 1))){
+					dp[i] = min(dp[j + 1] + 1, dp[i]);
+				}
+			}
+		}
+		return dp[0] ;
+	}
+}
 
-int minCut(string s){
+int main(){
+	string s("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
+	cout << "result: " << /*minCut(s) <<*/ '\n' << "palindrome(s): " << palindrome(s) << '\n' ;
 }
