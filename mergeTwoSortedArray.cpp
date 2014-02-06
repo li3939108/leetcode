35fa7432bcbb3137c97c050e5357a9a2e728caf1
Fanchy <li3939108@gmail.com>
2014-02-05 22:58:19 -0600

    add questions

--- mergeTwoSortedArray.cpp
+++ mergeTwoSortedArray.cpp
@@ -1,5 +1,9 @@
-// ConsoleApplication1.cpp : Defines the entry point for the console application.
-//
+/*
+Given two sorted integer arrays A and B, merge B into A as one sorted array.
+
+Note:
+You may assume that A has enough space to hold additional elements from B. The number of elements initialized in A and B are m and n respectively.
+*/
 
 #include "stdafx.h"
 #include <iostream>
