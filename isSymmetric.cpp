b0948768e210530e185341f55ae91aeee68a6857
Fanchy <li3939108@gmail.com>
2014-02-05 22:50:00 -0600

    add questions

--- isSymmetric.cpp
+++ isSymmetric.cpp
@@ -1,3 +1,20 @@
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+*/
 #include <iostream>
 #include <vector>
 
--- longestPalindrome.cpp
+++ longestPalindrome.cpp
@@ -1,3 +1,6 @@
+/*
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
 #include <iostream>
 #include <string>
 #include <map>
@@ -45,4 +48,3 @@ string longestPalindrome(string s) {
 	}
 	return s.substr(maxS, maxE - maxS + 1) ;
 }
-
